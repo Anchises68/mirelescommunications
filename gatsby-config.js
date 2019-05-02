b9915ee65fe7a15e899c3af94dc1785d3bcebc94
Alex Mireles <anchises1968@gmail.com>
2019-05-01 20:56:13 -0700

    added recaptcha

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,7 @@ module.exports = {
         ]
       }
     },
+    `gatsby-plugin-recaptcha`,
     `gatsby-plugin-offline`
   ]
 }
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { navigate } from "gatsby-link";
+import Recaptcha from "react-google-recaptcha";
+
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
 
 function encode(data) {
   return Object.keys(data)
@@ -15,6 +18,10 @@ class Contact extends React.Component {
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleRecaptcha = value => {
+    this.setState({ "g-recaptcha-response": value });
   };
 
   handleSubmit = e => {
@@ -39,7 +46,7 @@ class Contact extends React.Component {
         method="POST"
         action="/success/"
         data-netfliy="true"
-        data-netfliy-honeypot="bot-field"
+        data-netlify-recaptcha="true"
         onSubmit={this.handleSubmit}
         >
         <input type="hidden" name="form-name" value="contact" />
@@ -71,6 +78,11 @@ class Contact extends React.Component {
           <div className="control">
             <input id="form_botcheck" name="form_botcheck" className="form-control" type="hidden" value=""/>
             <button className="button is-dark" type="submit" data-loading-text="Please wait...">Submit</button>
+              <Recaptcha
+                ref="recaptcha"
+                sitekey={RECAPTCHA_KEY}
+                onChange={this.handleRecaptcha}
+              />
           </div>
         </div>
       </form>
